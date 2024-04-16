701f539984c1ee86743fb4052bdb4775a707c3d7
FukcThat <63652e636f6465@gmail.com>
2024-04-16 23:24:12 +0200

    FEAT: Password validation works FEAT: Password Requirements are shown and give color change cue for status STYLE: Made it look alright with the new addition

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 const passTxt = document.querySelector("#password");
 const passConfirmTxt = document.querySelector("#confirmPassword");
+const requirementLabels = document.querySelectorAll("#passwordRequirements li");
 
 passConfirmTxt.addEventListener("change", (e) => {
   if (passTxt.value !== passConfirmTxt.value) {
@@ -10,3 +11,36 @@ passConfirmTxt.addEventListener("change", (e) => {
     passConfirmTxt.setCustomValidity("");
   }
 });
+
+passTxt.addEventListener("input", function () {
+  const password = this.value;
+
+  // Check each requirement and update the corresponding label
+  requirementLabels.forEach((label) => {
+    const requirement = label.id;
+    const regex = getRegexForRequirement(requirement);
+    const isRequirementMet = regex.test(password);
+
+    if (isRequirementMet) {
+      label.classList.add("met");
+    } else {
+      label.classList.remove("met");
+    }
+  });
+});
+
+// Function to return the regex pattern for each requirement
+function getRegexForRequirement(requirement) {
+  switch (requirement) {
+    case "lowercase":
+      return /[a-z]/;
+    case "uppercase":
+      return /[A-Z]/;
+    case "number":
+      return /\d/;
+    case "special":
+      return /[!@#$%^&*_=\-[\]{}();:',."<>/?\\`~]/;
+    case "length":
+      return /^.{8,25}$/;
+  }
+}
